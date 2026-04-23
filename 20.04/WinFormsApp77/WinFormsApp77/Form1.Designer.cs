2432fcba503015baefc8a1285aa0facc13d369d5
romankhash <romankhash@gmail.com>
2026-04-23 13:49:22 +0300

    1

---
+++ 20.04/WinFormsApp77/WinFormsApp77/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WinFormsApp77
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            btnNext = new Button();
+            btnPrev = new Button();
+            sapper = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(138, 184);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(94, 85);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // btnNext
+            // 
+            btnNext.Location = new Point(150, 119);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(72, 59);
+            btnNext.TabIndex = 1;
+            btnNext.Text = "next";
+            btnNext.UseVisualStyleBackColor = true;
+            btnNext.Click += btnNext_Click;
+            // 
+            // btnPrev
+            // 
+            btnPrev.Location = new Point(150, 275);
+            btnPrev.Name = "btnPrev";
+            btnPrev.Size = new Size(72, 59);
+            btnPrev.TabIndex = 2;
+            btnPrev.Text = "previous";
+            btnPrev.UseVisualStyleBackColor = true;
+            btnPrev.Click += btnPrev_Click;
+            // 
+            // sapper
+            // 
+            sapper.Location = new Point(568, 199);
+            sapper.Name = "sapper";
+            sapper.Size = new Size(112, 51);
+            sapper.TabIndex = 3;
+            sapper.Text = "saper";
+            sapper.UseVisualStyleBackColor = true;
+            sapper.Click += btnStartSapper_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(sapper);
+            Controls.Add(btnPrev);
+            Controls.Add(btnNext);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button btnNext;
+        private Button btnPrev;
+        private Button sapper;
+    }
+}
---
+++ 20.04/WinFormsApp77/WinFormsApp77/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System.Windows.Forms;
+
+namespace WinFormsApp77
+{
+    public partial class Form1 : Form
+    {
+        private string[] memes = { "meme1.jpg", "meme2.jpg", "meme3.jpg" };
+        private int currentIndex = 0;
+
+        private const int size = 10;
+        private const int bombCount = 10;
+        private Button[,] cells = new Button[size, size];
+        private bool[,] bombs = new bool[size, size];
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            UpdateGallery();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (currentIndex < memes.Length - 1)
+            {
+                currentIndex++;
+                UpdateGallery();
+            }
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            if (currentIndex > 0)
+            {
+                currentIndex--;
+                UpdateGallery();
+            }
+        }
+
+        private void UpdateGallery()
+        {
+            try
+            {
+                pictureBox1.Image = Image.FromFile(memes[currentIndex]);
+                this.Text = $"Meme {currentIndex + 1}/{memes.Length}";
+            }
+            catch
+            {
+                MessageBox.Show("File not found!");
+            }
+        }
+
+        private void btnStartSapper_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            this.Width = 420;
+            this.Height = 450;
+
+            Random rnd = new Random();
+            int planted = 0;
+            while (planted < bombCount)
+            {
+                int x = rnd.Next(size);
+                int y = rnd.Next(size);
+                if (!bombs[x, y])
+                {
+                    bombs[x, y] = true;
+                    planted++;
+                }
+            }
+
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    Button btn = new Button
+                    {
+                        Size = new Size(40, 40),
+                        Location = new Point(x * 40, y * 40),
+                        Tag = new Point(x, y)
+                    };
+                    btn.Click += CellClick;
+                    cells[x, y] = btn;
+                    this.Controls.Add(btn);
+                }
+            }
+        }
+
+        private void CellClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Point p = (Point)btn.Tag;
+
+            if (bombs[p.X, p.Y])
+            {
+                btn.Text = "💣";
+                btn.BackColor = Color.Red;
+                MessageBox.Show("Boom!");
+                Application.Restart();
+            }
+            else
+            {
+                int count = CountNearby(p.X, p.Y);
+                btn.Text = count > 0 ? count.ToString() : "";
+                btn.Enabled = false;
+                btn.BackColor = Color.LightGray;
+            }
+        }
+
+        private int CountNearby(int x, int y)
+        {
+            int count = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    int nx = x + i, ny = y + j;
+                    if (nx >= 0 && nx < size && ny >= 0 && ny < size && bombs[nx, ny])
+                        count++;
+                }
+            }
+            return count;
+        }
+    }
+}
